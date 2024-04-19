35bd80c05c3f1961f9ef98a58bb248ca9d0d60a6
exg0rd <egor.sayapin1337@gmail.com>
2024-04-19 15:15:10 +0300

    обновление профиля без обновления страницы

--- src/pages/ProfilePage/ProfilePage.ts
+++ src/pages/ProfilePage/ProfilePage.ts
@@ -1,18 +1,15 @@
 import Base from '@components/Base/Base';
 import Header from '@components/Header/Header';
-import { UserProfile } from 'src/types/api';
 import { validate } from '@utils/validation';
 import AuthorizationForm from '@components/Form/AuthorizationForm';
 import { get, post } from '@api/base';
-import { router } from '@router/router';
 import JourneyPreview from './JourneyPreview';
 import { imageUpload } from '@api/user';
 import { signupErrors } from '../../types/errors';
 import { ROUTES } from '@router/ROUTES';
+import ProfileBlock from './ProfileBlock';
 
 class ProfilePage extends Base {
-
-  userdata : UserProfile;
 
   isOwn : boolean;
 
@@ -33,26 +30,22 @@ class ProfilePage extends Base {
     const profileData = await get(`profile/${this.userID}`);
 
     const authForm = new AuthorizationForm(this.parent);
-    let avatar;
-    if (profileData.data.avatar === '') {
-      avatar = '/static/placeholder.jpg';
-    } else {
-      avatar = profileData.data.avatar.replace(/.*\/public\//, '/public/');
-    }
+  
 
-    this.userdata = { id : profileData.data.id, username : profileData.data.username, status : profileData.data.bio, avatar : avatar };
+    const profileTemplateData = { id : profileData.data.id, username : profileData.data.username, status : profileData.data.bio, avatar : profileData.data.avatar };
     
     await this.preRender();
     const header = document.getElementById('header') as HTMLElement;
     document.body.classList.remove('auth-background');
     await new Header(header).render();
 
-    const profileSettingsButton = document.querySelector('#profile-edit-button') as HTMLButtonElement;
+    const profileBlock = document.querySelector('#profile-block') as HTMLDivElement; 
+
+    await new ProfileBlock(profileBlock, profileTemplateData).render();
+
+  
     const profileEditForm = document.querySelector('dialog') as HTMLDialogElement;
     
-    profileSettingsButton?.addEventListener('click', () => {
-      profileEditForm.showModal();
-    });
     const submitButton = document.getElementById('button-submit') as HTMLButtonElement;
 
     const cancelButton = document.querySelector('#button-cancel') as HTMLButtonElement;
@@ -68,9 +61,13 @@ class ProfilePage extends Base {
     ));
     const inputs = document.querySelectorAll('input') as NodeListOf<HTMLInputElement>;
 
-    const usernameField = inputs[0];
-    const passwordField = inputs[1];
+    const usernameField = inputs[0] as HTMLInputElement;
+    const passwordField = inputs[1] as HTMLInputElement;
+    const repeatPasswordField = inputs[2] as HTMLInputElement;
     const statusField = document.querySelector('textarea') as HTMLTextAreaElement;
+
+    usernameField.value = profileTemplateData.username;
+    statusField.value = profileTemplateData.status;
 
     const imageInput = document.querySelector('#profile-edit-avatar') as HTMLInputElement;
     let formData : FormData;
@@ -86,33 +83,46 @@ class ProfilePage extends Base {
       e.preventDefault();
       const input = document.querySelectorAll('.input')[2] as HTMLInputElement;
 
-      const profileRequestBody = { userID: this.userdata.id, username : usernameField.value, bio : statusField.value };
+      if (formData) {
+        imageUpload(ROUTES.profile.upload(this.userID), formData).then((imageUploadResponse) => {
+          if (imageUploadResponse.status !== 200) {
+            authForm.renderError(input, signupErrors[imageUploadResponse.data.error]);
+          }
+        });
+      }
+     
+      const profileRequestBody = { userID: this.userData.id, username : usernameField.value, bio : statusField.value };
+
       post(ROUTES.profile.edit(this.userID), profileRequestBody).then((profileBioNickEditResponse) => {
         if (profileBioNickEditResponse.status !== 200) {
           authForm.renderError(input, signupErrors[profileBioNickEditResponse.data.error]);
+        } else {
+          profileBlock.innerHTML = '';
+          const templateData = { id : profileBioNickEditResponse.data.id, 
+            username : profileBioNickEditResponse.data.username, 
+            status : profileBioNickEditResponse.data.bio, 
+            avatar : profileBioNickEditResponse.data.avatar };
+
+          new ProfileBlock(profileBlock, templateData).render(); // вынести в тип
         }
       });
 
-      post(ROUTES.profile.reset_password(this.userID), { password : passwordField.value }).then((passwordResponse) => {
-        if (passwordResponse.status === 401 && passwordField.value.length > 0) {
-          authForm.renderError(input, signupErrors[passwordResponse.data.error]);
-        }
-      });
-      
-      imageUpload(ROUTES.profile.upload(this.userID), formData).then((imageUploadResponse) => {
-        if (imageUploadResponse.status !== 200) {
-          authForm.renderError(input, signupErrors[imageUploadResponse.data.error]);
-        }
-      });
-
+      if (passwordField.value.length > 0 && passwordField.value === repeatPasswordField.value) {
+        post(ROUTES.profile.reset_password(this.userID), { password : passwordField.value }).then((passwordResponse) => {
+          if (passwordResponse.status === 401) {
+            authForm.renderError(input, signupErrors[passwordResponse.data.error]);
+          } 
+        });
+      }
+     
       if (document.querySelectorAll('.has-error').length === 0) {
-        router.go(ROUTES.profile.view(this.userID));
+        profileEditForm.close();
       }
     },
 
     );
 
-    const journeyList = await get(`${this.userdata.id}/trips`);
+    const journeyList = await get(`${this.userID}/trips`);
     if (journeyList.status === 200 && journeyList.data.journeys !== null) {
       const journeyDiv = document.querySelector('.profile-journeys ol') as HTMLDivElement;
       journeyDiv.innerHTML = '';
